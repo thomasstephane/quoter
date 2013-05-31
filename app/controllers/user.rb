c07a581e444430ee3b2da5b43971d3b851098469
Stephane Thomas <thomas.stephn@gmail.com>
2013-05-30 23:04:45 -0700

    Organized navbar into div for futur jquerysation app/views/_nav.erb
    Cleaned and rearranged routes for user
    app/controllers/user.rb

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -11,19 +11,15 @@ end
 
 #----------- USERS -----------
 
-get '/user/new' do
-  erb :sign_up
-end
-
-post '/user' do
-  create_user(params[:user])
-  redirect '/'
-end
-
 post '/user/uniq' do
   content_type :json
   puts "in the post uniq"
   puts params.inspect
   puts check_exist(params[:user]).inspect
   check_exist(params[:user]).to_json
+end
+
+post '/user' do
+  create_user(params[:user])
+  redirect '/'
 end
