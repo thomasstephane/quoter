754477e3b4e1a6d0c01b0c6e3f1646cc40d9cb19
Stephane Thomas <thomas.stephn@gmail.com>
2013-05-31 01:18:47 -0700

    Create new customer view and route app/controllers/quote.rb app/views/_customer.erb
    Link created on the webpage
    app/views/index.erb
    
    Field user_id added to a quote - need to drop and recreate DB.
    db/migrate/20130530081546_create_quotes.rb

--- app/controllers/quote.rb
+++ app/controllers/quote.rb
@@ -2,3 +2,12 @@ get '/quote' do
   @date_default = Date.today
   erb :quote
 end
+
+post '/quote' do 
+  @quote = Quote.create(params[:quote])
+  erb :_customer
+end
+
+get '/customer' do 
+  erb :_customer
+end
--- db/migrate/20130530081546_create_quotes.rb
+++ db/migrate/20130530081546_create_quotes.rb
@@ -2,7 +2,7 @@ class CreateQuotes < ActiveRecord::Migration
   def change
     create_table :quotes do |t|
       t.string :ref, :currency, :discount, :status, :comment
-      t.integer :person_id
+      t.integer :person_id, :user_id
       t.date :submitted_at, :due_at, :approved_at, :closed_at, :follow_up_at
       t.timestamps
     end
